22cfe8ab68a7e0c32ccbdd7953ba00688378c592
BryanWille <84272231+BryanWille@users.noreply.github.com>
2021-10-19 18:40:34 -0300

    colocando o scroll up - teste 1

--- trabalho2/js/script.js
+++ trabalho2/js/script.js
@@ -1,36 +1,16 @@
-const scrollReveal = ScrollReveal({
-    origin: "top",
-    distance: "30px",
-    duration: 700,
-    reset: true,
-  });
-  
-  /* ao usar crase para montar strings, é possível quebrar linhas */
-  scrollReveal.reveal(
-    ` 
-      .banner-elementos h1, .banner-elementos p, .banner-elementos button,
-      #como-funciona h2, #como-funciona i, #como-funciona h3, #como-funciona p, #como-funciona .btn,
-      #destinos img, #destinos .card-title, #destinos .card-text, #destinos .btn,
-      #ajuda h2, #ajuda p, #ajuda .btn,
-      #destinos-internacionais h2, #destinos-internacionais .thumbnail,
-      footer h2, footer p, footer .btn
-    `,
-    { interval: 100 }
-  );
-  
-  /* botão voltar para o topo */
-  const backToTopButton = document.querySelector(".back-to-top");
-  
-  function backToTop() {
-    if (this.window.scrollY >= 560) {
-      backToTopButton.classList.add("show");
-    } else {
-      backToTopButton.classList.remove("show");
-    }
+mybutton = document.getElementById("topButton");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-  
-  /* When Scroll */
-  window.addEventListener("scroll", function () {
-    backToTop();
-  });
-  
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
